5a1aad3bed988d4a9cac049c798955033cc52a9e
alireza-gnf <alirezagnf99@gmail.com>
2024-07-17 00:29:15 +0330

    fix status code

--- src/main.ts
+++ src/main.ts
@@ -66,7 +66,7 @@ app.post("/login", (req, res) => {
 
 // GET Visions Endpoint
 app.get("/visions", (req, res) => {
-  res.status(201).json({ data: visions.all() });
+  res.status(200).json({ data: visions.all() });
 });
 
 // POST Vision Endpoint
